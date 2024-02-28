239c91bec0a1f64b1fe7caaf5c730e6e01804aa0
arodriguezla <arodriguezla@est.utn.ac.cr>
2024-02-27 19:46:16 -0600

    2

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+          <nav>
+          <img
+                    className="img-fluid"
+                    src="/logoPk.jpg"
+                    alt="" />
+          </nav>
+          <hr />
+          <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+        <button className="btn btn-primary me-md-2" type="button">Elige la generación</button>
+        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Primera Generación</a></li>
+            <li><a className="dropdown-item" href="#">Segunda Generación</a></li>
+            <li><a className="dropdown-item" href="#">Tercera Generación</a></li>
+            <li><a className="dropdown-item" href="#">Cuarta Generación</a></li>
+            <li><a className="dropdown-item" href="#">Quinta Generación</a></li>
+        </ul>
+          
+          
+          </div>
+          <br /><br />
+
+         
+
+          <div className="container text-center">
+            <div className="row">
+              <div className="col">
+                <img src="PrimeraGeneracion" alt="" />
+                Column
+              </div>
+              <div className="col">
+                Column
+              </div>
+              <div className="col">
+                Column
+              </div>
+              <div className="col">
+                Column
+              </div>
+              <div className="col">
+                Column
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                Column
+              </div>
+              <div className="col">
+                Column
+              </div>
+              <div className="col">
+                Column
+              </div>
+              <div className="col">
+                Column
+              </div>
+              <div className="col">
+                Column
+              </div>
+            </div>
+            
+          </div>
+
+
+            
+        </div>
+    );
 }
 
 export default App;
